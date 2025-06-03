276a68fa16fe44dba71446bfe98d611cc861612b
Daniel Mangoubi <162180456+DMangoubiHBS@users.noreply.github.com>
2025-06-03 13:53:38 -0400

    Deleted unneeded file

--- LogOn/WRDSLogOn.R
+++
@@ -1,8 +0,0 @@
-## Log into WRDS
-wrds <- dbConnect(Postgres(),
-                  host='wrds-pgdata.wharton.upenn.edu',
-                  port=9737,
-                  dbname='wrds',
-                  sslmode='require',
-                  user='YOUR_USERNAME',  #Update to your username
-                  password='YOUR_PASSWORD') #Update to your password
